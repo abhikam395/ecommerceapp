87c82683f4888081688f91435e57960087a13cf1
abhikam395 <--add>
2021-05-30 14:39:41 +0530

    working on home page

---
+++ src/components/SaleItemComponent.js
@@ -0,0 +1,147 @@
+import React, {Component} from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Rating} from 'react-native-ratings';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA_djpm0QdOAgvKxQhB3EZuT6JPHn1FcSDaw&usqp=CAU';
+
+export default class SaleItemComponent extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            favorite: false
+        }
+        this.addToFavorite = this.addToFavorite.bind(this);
+        this.ratingCompleted = this.ratingCompleted.bind(this);
+    }
+
+    ratingCompleted(rating) {
+        console.log("Rating is: " + rating)
+      }
+
+    addToFavorite(){
+        let {favorite} = this.state;
+        this.setState({favorite: !favorite});
+    }
+    
+    render(){
+        let {title} = this.props;
+        let {favorite} = this.state;
+        let icon = favorite ? 'favorite' : 'favorite-outline'
+        return (
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image source={{uri: imageUrl}} style={styles.image}/>
+                    <TouchableOpacity 
+                        style={styles.favoriteButton} 
+                        onPress={this.addToFavorite}>
+                        <MaterialIcons name={icon} size={22} color={favorite ? 'red' : 'grey'}/>
+                    </TouchableOpacity>
+                    <View style={styles.discountContainer}>
+                        <Text style={styles.discount}>-20%</Text>
+                    </View>
+                    <View style={styles.ratingContainer}>
+                        <Rating
+                            type='star'
+                            ratingCount={5}
+                            imageSize={16}
+                            readonly
+                            jumpValue={1}
+                            fractions={1}
+                            startingValue={3}
+                            style={styles.rating}
+                        />
+                        <Text style={styles.ratingCount}>(10)</Text>
+                    </View>
+                    <Text style={styles.seller}>Dorothy Perkins</Text>
+                    <Text style={styles.title}>Evening Dress</Text>
+                    <View style={styles.priceContainer}>
+                        <Text style={styles.previousPrice}>15$</Text>
+                        <Text style={styles.newPrice}>12$</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 180,
+        height: 360,
+        borderRadius: 10,
+    },
+    imageContainer: {
+        height: 250,
+    },
+    image: {
+        height: '100%',
+        borderRadius: 10
+    },
+    favoriteButton: {
+        height: 46,
+        width: 46,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderRadius: 23,
+        position: 'absolute',
+        bottom: -25,
+        right: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    discountContainer: {
+        backgroundColor: 'red',
+        height: 30,
+        width: 50,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        margin: 10
+    },
+    discount: {
+        color: 'white',
+        fontSize: 12,
+        fontWeight: 'bold',
+        letterSpacing: 2
+    },
+    ratingContainer: {
+        alignSelf: 'flex-start',
+        marginVertical: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    rating: {
+        color: "white",
+        backgroundColor: "white"
+    },
+    ratingCount: {
+        marginLeft: 10,
+        fontSize: 12
+    },
+    seller: {
+        fontSize: 12
+    },  
+    title: {
+        fontSize: 18,
+        color: 'black',
+        fontWeight: 'bold'
+    },  
+    priceContainer: {
+        flexDirection: 'row'
+    },
+    previousPrice: {
+        fontSize: 16,
+        textDecorationLine: 'line-through',
+        fontWeight: 'bold',
+    },
+    newPrice: {
+        fontSize: 16,
+        color: 'red',
+        fontWeight: 'bold',
+        marginLeft: 5
+    }
+})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,157 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, StatusBar, TouchableOpacity, FlatList, ScrollView } from 'react-native';
+import NewItemComponent from '../components/NewItemComponent';
+import SaleItemComponent from '../components/SaleItemComponent';
+
+const imageUrl = 'https://img.freepik.com/free-photo/dark-haired-woman-with-red-lipstick-smiles-leans-stand-with-clothes-holds-package-pink-background_197531-17609.jpg?size=626&ext=jpg';
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
 
 export default class HomeScreen extends Component{
+
+    constructor(){
+        super();
+        this.renderSaleItem = this.renderSaleItem.bind(this);
+        this.renderNewItem = this.renderNewItem.bind(this);
+    }
+
+    renderSaleItem({item}){
+        return <SaleItemComponent title={item.title}/>;
+    }
+
+    renderNewItem({item}){
+        return <NewItemComponent title={item.title}/>;
+    }
+
     render(){
         return (
-            <View style={styles.container}>
-                <Text>HomeScreen</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <StatusBar translucent backgroundColor="transparent" />
+                <View style={styles.header}>
+                    <Image source={{uri: imageUrl}} style={styles.headerImage}/>
+                    <Text style={styles.headerTitle}>Street clothes</Text>
+                </View>
+                <View style={styles.saleContainer}>
+                    <View style={styles.row}>
+                        <Text style={styles.saleTitle}>Sale</Text>
+                        <TouchableOpacity style={styles.viewAll}>
+                            <Text style={styles.viewAllText}>View All</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.label}>Super summer sale</Text>
+                    <FlatList
+                        data={DATA}
+                        renderItem={this.renderSaleItem}
+                        contentContainerStyle={{marginVertical: 30}}
+                        keyExtractor={item => item.id}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        ItemSeparatorComponent={
+                            () => <View style={{width: 20}}/>
+                        }
+                        >
+
+                    </FlatList>
+                </View>
+                <View style={styles.newClothsContainer}>
+                    <View style={styles.row}>
+                        <Text style={styles.newClothTitle}>New</Text>
+                        <TouchableOpacity style={styles.viewAll}>
+                            <Text style={styles.viewAllText}>View All</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.label}>You'ne never seen it before!</Text>
+                    <FlatList
+                        data={DATA}
+                        renderItem={this.renderNewItem}
+                        contentContainerStyle={{marginVertical: 30}}
+                        keyExtractor={item => item.id}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        ItemSeparatorComponent={
+                            () => <View style={{width: 20}}/>
+                        }
+                        >
+
+                    </FlatList>
+                </View>
+            </ScrollView>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
-    }
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    header: {
+        height: 250,
+        width: '100%'
+    },
+    headerImage: {
+        height: '100%'
+    },
+    headerTitle: {
+        position: 'absolute',
+        bottom: 10,
+        padding: 20,
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    saleContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 10
+    },
+    saleTitle: {
+        fontSize: 33,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    viewAll: {
+        marginLeft: 'auto',
+    },
+    viewAllText: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    newClothsContainer: {
+        // paddingVertical: 10,
+        paddingHorizontal: 10
+    },
+    newClothTitle: {
+        fontSize: 33,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    viewAll: {
+        marginLeft: 'auto',
+    },
+    viewAllText: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
 })
