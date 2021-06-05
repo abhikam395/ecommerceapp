13784bfe11b2682ddb4687711b47be7b4f52e186
abhikam395 <--add>
2021-06-06 01:58:13 +0530

    option view added

--- src/components/MyBagProductItem.js
+++ src/components/MyBagProductItem.js
@@ -5,16 +5,19 @@ import Entypo from 'react-native-vector-icons/Entypo';
 
 const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA_djpm0QdOAgvKxQhB3EZuT6JPHn1FcSDaw&usqp=CAU';
 
-export default class MyBagProductItem extends Component{
+export default class    MyBagProductItem extends Component{
 
     constructor(){
         super();
         this.state = {
             price: 30,
-            quantity: 1
+            quantity: 1,
+            showOptionContainer: false,
         }
         this.increaseQuantity = this.increaseQuantity.bind(this);
         this.decreaseQuantity = this.decreaseQuantity.bind(this);
+        this.showOptions = this.showOptions.bind(this);
+        this.optionSelected = this.optionSelected.bind(this);
     }
 
     increaseQuantity(){
@@ -28,16 +31,34 @@ export default class MyBagProductItem extends Component{
             this.setState({quantity: quantity - 1});
     }
 
+    showOptions(){
+        this.setState({showOptionContainer: true});
+    }
+
+    optionSelected(){
+        this.setState({showOptionContainer: false})
+    }
+
     render(){
         return (
             <View style={styles.container}>
                 <Image style={styles.image} source={{uri: imageUrl}}/>
                 <View style={{padding: 10, flex: 1}}>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={this.showOptions} 
+                        style={{
+                            position: 'absolute', 
+                            right: 10, 
+                            width: 40, 
+                            height: 40,
+                            padding: 10,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
                         <Entypo 
                             name="dots-three-vertical" 
-                            size={20} color="lightgrey" 
-                            style={{ position: 'absolute', right: 0}}/>
+                            size={24} 
+                            color="lightgrey"
+                        />
                     </TouchableOpacity>
                     <Text style={{fontSize: 18, color: 'black', fontWeight: 'bold'}}>Pullover</Text>
                     <View style={{flexDirection: 'row'}}>
@@ -57,7 +78,7 @@ export default class MyBagProductItem extends Component{
                                 style={{
                                     height: 36, 
                                     width: 36, 
-                                    elevation: 5,  
+                                    elevation: 1,  
                                     borderRadius: 18, 
                                     justifyContent: 'center', 
                                     alignItems: 'center',
@@ -71,7 +92,7 @@ export default class MyBagProductItem extends Component{
                                 style={{
                                     height: 36, 
                                     width: 36, 
-                                    elevation: 5,  
+                                    elevation: 1,  
                                     borderRadius: 18, 
                                     justifyContent: 'center', 
                                     alignItems: 'center',
@@ -86,6 +107,21 @@ export default class MyBagProductItem extends Component{
                         </Text>
                     </View>
                 </View>
+                {this.state.showOptionContainer && (
+                    <View style={styles.optionContainer}>
+                        <TouchableOpacity
+                            onPress={this.optionSelected}
+                            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <Text style={{ fontSize: 13}}>Add to favorites</Text>  
+                        </TouchableOpacity>
+                        <View style={{height: 1, backgroundColor: 'lightgrey'}}/>
+                        <TouchableOpacity
+                            onPress={this.optionSelected} 
+                            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <Text style={{ fontSize: 13}}>Delete from the list</Text>  
+                        </TouchableOpacity>
+                    </View>
+                )}
             </View>
         )
     }
@@ -104,5 +140,15 @@ const styles = StyleSheet.create({
         width: 120,
         borderTopLeftRadius: 10,
         borderBottomLeftRadius: 10
+    },
+    optionContainer: {
+        backgroundColor: 'white',
+        width: 180,
+        height: 100,
+        position: 'absolute',
+        bottom: 30,
+        right: 50,
+        elevation: 10,
+        borderRadius: 15,
     }
 })
