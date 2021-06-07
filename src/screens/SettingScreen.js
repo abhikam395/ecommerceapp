f887ae619ba971f13e85620e158c0a6c5990dafd
abhikam395 <--add>
2021-06-07 16:36:34 +0530

    setting screen completed

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
-import { DatePickerIOSComponent, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import SettingNotificationItem from '../components/SettingNotificationItem';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DatePicker from 'react-native-date-picker'
 
 export default class SettingScreen extends Component{
 
     constructor(){
         super();
         this.state = {
-            showDateDialog: false
+            dateDialogVisible: false,
+            oldDate: new Date(),
+            updatedDate: new Date(),
         }
         this.showDateDialog = this.showDateDialog.bind(this);
         this.onDateChange = this.onDateChange.bind(this);
-        this.currentDate = new Date();
+        this.onDateSave = this.onDateSave.bind(this);
+        this.onDateCancel = this.onDateCancel.bind(this);
     }
 
     showDateDialog(){
-        this.setState({showDateDialog: true})
+        this.setState({dateDialogVisible: true});
     }
 
-    onDateChange(){
-        console.log(1)
+    onDateChange(date){
+        this.setState({updatedDate: date})
+    }
+
+    onDateSave(){
+        this.setState({dateDialogVisible: false});
+    }
+
+    onDateCancel(){
+        this.setState({updatedDate: this.state.oldDate, dateDialogVisible: false})
     }
 
     render(){
+        let {dateDialogVisible} = this.state;
         return (
             <View style={styles.container}>
                 <View style={styles.subContainer}>
@@ -34,7 +46,7 @@ export default class SettingScreen extends Component{
                     <TouchableOpacity 
                         style={styles.inputContainer}
                         onPress={this.showDateDialog}>
-                        <Text style={styles.input}>Date of Birth</Text>
+                        <Text style={styles.input}>{this.state.updatedDate.toDateString()}</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={styles.subContainer}>
@@ -59,12 +71,26 @@ export default class SettingScreen extends Component{
                     <SettingNotificationItem title="New arrivals"/>
                     <SettingNotificationItem title="Delivery status changes"/>
                 </View>
-                {this.state.showDateDialog && (
-                    <DateTimePicker
-                    mode="date"
-                    minimumDate={new Date()}
-                    maximumDate={new Date()}
-                  />
+                {dateDialogVisible && (
+                    <View style={styles.dateContainer}>
+                        <DatePicker
+                            mode="date"
+                            date={this.state.updatedDate}
+                            onDateChange={this.onDateChange}
+                        />
+                        <View style={styles.dateButtonsContainer}>
+                            <TouchableOpacity 
+                                style={styles.cancelButton}
+                                onPress={this.onDateCancel}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity 
+                                style={styles.saveButton}
+                                onPress={this.onDateSave}>
+                                <Text style={styles.buttonText}>Save</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 )}
             </View>
         )
@@ -105,4 +131,42 @@ const styles = StyleSheet.create({
     changeLabel: {
         fontSize: 14
     },
+    dateContainer: {
+        position: 'absolute',
+        top: 100,
+        left: 30,
+        zIndex: 10,
+        elevation: 5,
+        backgroundColor: 'white',
+        margin: 'auto',
+        padding: 10
+    },
+    saveButton: {
+        height: 40,
+        width: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        marginLeft: 20,
+        backgroundColor: '#43a047',
+        elevation: 5
+    },
+    cancelButton: {
+        height: 40,
+        width: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        elevation: 5,
+        backgroundColor: 'red'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    dateButtonsContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        alignSelf: 'center',
+    }
 })
